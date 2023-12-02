71036fb3c25244f6d807a024a01250be3cd8a465
WilliamMasterman <williammasterman1233@gmail.com>
2023-12-01 21:42:38 -0500

    Adding socstring to definitions

--- ToDoList_Final.py
+++ ToDoList_Final.py
@@ -1,5 +1,16 @@
 class Task:
     def __init__(self, title, due_date, description, category):
+        """
+        This represents an individual task
+
+        Returns: None
+
+        Arguments:
+            title (str): Title of the task.
+            due_date (datetime): Due date of the task
+            description (str): Description of the task
+            category (str): Category of the task
+        """
         self.title = title
         self.due_date = due_date
         self.description = description
@@ -7,6 +18,7 @@ class Task:
         self.completed = False
         
     def edit_task(self, new_title, new_due_date, new_description, new_category):
+        ""
         self.title = new_title
         self.due_date = new_due_date
         self.description = new_description
