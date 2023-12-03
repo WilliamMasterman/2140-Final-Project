3772236027a1a772b6a69c2dc01213528318a091
WilliamMasterman <williammasterman1233@gmail.com>
2023-12-02 19:11:57 -0500

    added test for making complete

--- ToDoList_test.py
+++ ToDoList_test.py
@@ -25,6 +25,19 @@ class TestTaskClass(unittest.TestCase):
         self.assertEqual(task.category, self.task_category)
         self.assertFalse(task.completed)
 
+    def test_task_completion(self):
+        #test for marking a singular task complete
+        task = Task(
+            self.task_title,
+            self.task_due_date,
+            self.task_description,
+            self.task_category
+        )
+
+        self.assertFalse(task.completed)
+        task.completed = True
+        self.assertTrue(task.completed)
+
 
 if __name__ == '__main__':
     unittest.main()
