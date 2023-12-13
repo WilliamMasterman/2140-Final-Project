18f5a491d19fcc7161aa2f4b40f306e54e8f70ca
WilliamMasterman <williammasterman1233@gmail.com>
2023-12-13 17:35:08 -0500

    started testing for interactive app

--- TextBasedToDoListApp_test.py
+++ TextBasedToDoListApp_test.py
@@ -1,1 +1,11 @@
+import unittest
+from unittest.mock import patch
+from ToDoList_Final import TextBasedToDoListApp
 
+class TestTextBasedToDoListApp(unittest.TestCase):
+    def setUp(self):
+        self.app = TextBasedToDoListApp()
+
+
+if __name__ == '__main__':
+    unittest.main()
