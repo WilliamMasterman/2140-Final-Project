deeec8839fd5a9912bba65fe73541a892100b0be
WilliamMasterman <williammasterman1233@gmail.com>
2023-12-13 15:19:47 -0500

    added logic for tasklist

--- TaskList_test.py
+++ TaskList_test.py
@@ -6,6 +6,19 @@ class TestTaskList(unittest.TestCase):
     def setUp(self):
         self.task_list = TaskList()
 
+    def test_mark_task_complete(self):
+        #create task and add to task list
+        task_title = "Test Task"
+        task_due_date = datetime.now()
+        task_description = "Test Description"
+        task_category = "Test Category"
+        task = Task(task_title, task_due_date, task_description, task_category)
+        self.task_list.add_task(task)
+
+        #mark task complete
+        self.task_list.mark_task_complete(task_title)
+
+
 
 if __name__ == '__main__':
     unittest.main()
--- ToDoList_test.py
+++ ToDoList_test.py
@@ -4,7 +4,7 @@ from ToDoList_Final import Task
 
 class TestTaskClass(unittest.TestCase):
     def setUp(self):
-        # Initialize common data for tests
+        #initialize common data for tests
         self.task_title = "Sample Task"
         self.task_due_date = datetime(2023, 1, 1)
         self.task_description = "This is a sample task description."
