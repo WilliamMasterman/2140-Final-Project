66cf1a081ee4a27a4d51a61037499d9620191a81
WilliamMasterman <williammasterman1233@gmail.com>
2023-12-13 18:04:26 -0500

    added test for adding special proj

--- TextBasedToDoListApp_test.py
+++ TextBasedToDoListApp_test.py
@@ -28,6 +28,14 @@ class TestTextBasedToDoListApp(unittest.TestCase):
             self.app.delete_task_in_project_special_project()
             mock_delete_task_in_project.assert_called_once_with('Test Project', 'Test Task')
 
+    @patch('builtins.input', side_effect=['Special Project 1'])
+    def test_add_special_project(self, mock_input):
+        with patch.object(self.app, 'add_special_project') as mock_add_special_project:
+            self.app.add_special_project()
+
+            #ensures that add_special_project method is called
+            mock_add_special_project.assert_called_once()
+
 
 
 if __name__ == '__main__':
