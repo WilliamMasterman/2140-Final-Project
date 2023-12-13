b793ebbeb4c01f0366532232cba3140d4c5e0678
WilliamMasterman <williammasterman1233@gmail.com>
2023-12-13 15:48:20 -0500

    added new toest to delete special project

--- SpecialProjects_test.py
+++ SpecialProjects_test.py
@@ -7,9 +7,19 @@ class TestSpecialProjects(unittest.TestCase):
         self.special_project = SpecialProjects()
 
     def test_add_special_project(self):
+        #tests the creation of a special project
         project_name = "Test Project"
         self.special_project.categories[project_name] = []
         self.assertIn(project_name, self.special_project.categories)
+
+    def test_delete_special_project(self):
+        #tests deleting a special project
+        project_name = "Test Project"
+        self.special_project.categories[project_name] = []
+        self.special_project.delete_special_project(project_name)
+        self.assertNotIn(project_name, self.special_project.categories)
+
+
 
 if __name__ == '__main__':
     unittest.main()
