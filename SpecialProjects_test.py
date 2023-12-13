0ff99051bc33eca80b7d56c083c30f0d88c30b32
WilliamMasterman <williammasterman1233@gmail.com>
2023-12-13 15:53:43 -0500

    testing adding task to project started view

--- SpecialProjects_test.py
+++ SpecialProjects_test.py
@@ -19,6 +19,23 @@ class TestSpecialProjects(unittest.TestCase):
         self.special_project.delete_special_project(project_name)
         self.assertNotIn(project_name, self.special_project.categories)
 
+    def test_add_task_to_project(self):
+        task_title = "Test Task"
+        task_due_date = datetime(2023, 1, 1)
+        task_description = "Test Description"
+        task_category = "Test Category"
+        task = Task(task_title, task_due_date, task_description, task_category)
+
+        project_name = "Test Project"
+        self.special_project.categories[project_name] = []
+        self.special_project.add_task_to_project(task, project_name)
+
+        self.assertIn(task, self.special_project.categories[project_name])
+
+    def test_view_tasks_in_project(self):
+        task_title = "Test Task"
+        task_due_date = datetime(2023, 1, 1)
+        task_description = "Test Description"
 
 
 if __name__ == '__main__':
