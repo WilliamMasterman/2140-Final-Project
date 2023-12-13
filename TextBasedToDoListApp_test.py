b85ee05bc432e9bb58af027dd67e1dd16c4132a9
WilliamMasterman <williammasterman1233@gmail.com>
2023-12-13 17:48:56 -0500

    test for deleting task in special proj added

--- TextBasedToDoListApp_test.py
+++ TextBasedToDoListApp_test.py
@@ -14,6 +14,14 @@ class TestTextBasedToDoListApp(unittest.TestCase):
             #ensures delete_special_project method is called with correct arguments
             mock_delete_special_project.assert_called_once_with('Special Project 1')
 
+    @patch('builtins.input', side_effect=['Special Project 1', 'Task Title 1'])
+    def test_delete_task_in_project_special_project(self, mock_input):
+        with patch.object(self.app.special_project, 'delete_task_in_project') as mock_delete_task_in_project:
+            self.app.delete_task_in_project_special_project()
+
+            #ensures delete_task_in_project method is called with correct arguments
+            mock_delete_task_in_project.assert_called_once_with('Special Project 1', 'Task Title 1')
+
 
 
 if __name__ == '__main__':
